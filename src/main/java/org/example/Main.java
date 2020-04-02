8b013d52f8f196a6f1bd7193b58a12c04d09a44c
AzatBaisynov <azat.baisynov@gmail.com>
2020-04-02 23:27:54 +0600

    ''

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,12 @@ package org.example;
 
 
 import org.example.controller.Bot;
-import org.example.controller.Jfr;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
     public static void main(String[] args) {
-        Jfr.createJfr();
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
