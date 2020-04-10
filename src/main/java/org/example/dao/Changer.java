c13c398794f244e5ec44dbfb8139c93676071001
AzatBaisynov <azat.baisynov@gmail.com>
2020-04-10 17:49:37 +0600

    ''

--- src/main/java/org/example/dao/Changer.java
+++ src/main/java/org/example/dao/Changer.java
@@ -1,8 +1,46 @@
 package org.example.dao;
 
+import org.example.repo.Article;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.*;
+
 public class Changer {
-    static String stringChanger(String str){
-        str.replaceAll("<>tr<>", "");
-        return str;
+    static List kyrgyzstanList(){
+        Document doc = null;
+        try { doc = Jsoup.connect("https://www.worldometers.info/coronavirus/").get(); }
+        catch (IOException e) { e.printStackTrace(); }
+
+        LinkedList<String> list = new LinkedList<>();
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        String s = doc.getElementsByTag("td").text();
+        String sub = s.substring(s.indexOf("Kyrgyzstan"), s.length());
+        String sub1 = sub.substring(sub.indexOf("Kyrgyzstan"), sub.indexOf("Asia"));
+        String[] sub2 = sub1.split(" ");
+        for (int i = 0; i < sub2.length; i++){
+            set.add(sub2[i]);
+        }
+        set.forEach(x -> list.add(x));
+        return list;
+    }
+
+    static List russiaList(){
+        Document doc = null;
+        try { doc = Jsoup.connect("https://www.worldometers.info/coronavirus/").get(); }
+        catch (IOException e) { e.printStackTrace(); }
+
+        LinkedList<String> list = new LinkedList<>();
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        String s = doc.getElementsByTag("td").text();
+        String sub = s.substring(s.indexOf("Russia"), s.length());
+        String sub1 = sub.substring(sub.indexOf("Russia"), sub.indexOf("Europe"));
+        String[] sub2 = sub1.split(" ");
+        for (int i = 0; i < sub2.length; i++){
+            set.add(sub2[i]);
+        }
+        set.forEach(x -> list.add(x));
+        return list;
     }
 }
--- src/main/java/org/example/dao/HtmlReader.java
+++ src/main/java/org/example/dao/HtmlReader.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class HtmlReader {
@@ -57,108 +58,34 @@ public class HtmlReader {
 
 
     public static Article getKyrgyzOfficialTable() {
-        int country = 104;
-        int totalCases = 105;
-        int newCases = 106;
-        int totalDeaths = 107;
-        int newDeaths = 108;
-        int totalRecovered = 109;
-        int activeCases = 110;
-        List<Article> articles = new ArrayList<>();
-        Document doc = null;
-        try {
-            doc = Jsoup.connect("https://www.worldometers.info/coronavirus/").get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        while (0 < 1) {
-            articles.add(new Article(
-                    doc.getElementsByTag("td").get(country).text(),
-                    checkForNull(doc.getElementsByTag("td").get(totalCases).text()),
-                    checkForNull(doc.getElementsByTag("td").get(newCases).text()),
-                    checkForNull(doc.getElementsByTag("td").get(totalDeaths).text()),
-                    checkForNull(doc.getElementsByTag("td").get(newDeaths).text()),
-                    checkForNull(doc.getElementsByTag("td").get(totalRecovered).text()),
-                    checkForNull(doc.getElementsByTag("td").get(activeCases).text())));
-            country += 13;
-            totalCases += 13;
-            newCases += 13;
-            totalDeaths += 13;
-            newDeaths += 13;
-            totalRecovered += 13;
-            activeCases += 13;
-            if (doc.getElementsByTag("td").get(country).text().contains("Total:")){
-                break;
-            }
-        }
-
-
-//        kyrgystan.setCountry(doc.getElementsByTag("td").get(country).text());
-//        kyrgystan.setTotalCases(checkForNull(doc.getElementsByTag("td").get(country+1).text()));
-//        kyrgystan.setNewCases(checkForNull(doc.getElementsByTag("td").get(country+2).text()));
-//        kyrgystan.setTotalDeaths(checkForNull(doc.getElementsByTag("td").get(country+3).text()));
-//        kyrgystan.setNewDeaths(checkForNull(doc.getElementsByTag("td").get(country+4).text()));
-//        kyrgystan.setTotalRecovered(checkForNull(doc.getElementsByTag("td").get(country+5).text()));
-//        kyrgystan.setActiveCases(checkForNull(doc.getElementsByTag("td").get(country+6).text()));
-        for (Article a: articles){
-            if(a.getCountry().contains("Kyrgyzstan"))
-                return a;
-        }
-        return null;
+        List<String> list = Changer.kyrgyzstanList();
+        Article article = new Article(
+                list.get(0),
+                list.get(1),
+                checkForNull(list.get(2)),
+                list.get(3),
+                checkForNull(list.get(4)),
+                list.get(5),
+                list.get(6)
+        );
+        return article;
     }
 
     public static Article getRussiaOfficialTable() {
-        int country = 104;
-        int totalCases = 105;
-        int newCases = 106;
-        int totalDeaths = 107;
-        int newDeaths = 108;
-        int totalRecovered = 109;
-        int activeCases = 110;
-        List<Article> articles = new ArrayList<>();
-        Document doc = null;
-        try {
-            doc = Jsoup.connect("https://www.worldometers.info/coronavirus/").get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        while (0 < 1) {
-            articles.add(new Article(
-                    doc.getElementsByTag("td").get(country).text(),
-                    checkForNull(doc.getElementsByTag("td").get(totalCases).text()),
-                    checkForNull(doc.getElementsByTag("td").get(newCases).text()),
-                    checkForNull(doc.getElementsByTag("td").get(totalDeaths).text()),
-                    checkForNull(doc.getElementsByTag("td").get(newDeaths).text()),
-                    checkForNull(doc.getElementsByTag("td").get(totalRecovered).text()),
-                    checkForNull(doc.getElementsByTag("td").get(activeCases).text())));
-            country += 13;
-            totalCases += 13;
-            newCases += 13;
-            totalDeaths += 13;
-            newDeaths += 13;
-            totalRecovered += 13;
-            activeCases += 13;
-            if (doc.getElementsByTag("td").get(country).text().contains("Total:")){
-                break;
-            }
-        }
-
-
-//        kyrgystan.setCountry(doc.getElementsByTag("td").get(country).text());
-//        kyrgystan.setTotalCases(checkForNull(doc.getElementsByTag("td").get(country+1).text()));
-//        kyrgystan.setNewCases(checkForNull(doc.getElementsByTag("td").get(country+2).text()));
-//        kyrgystan.setTotalDeaths(checkForNull(doc.getElementsByTag("td").get(country+3).text()));
-//        kyrgystan.setNewDeaths(checkForNull(doc.getElementsByTag("td").get(country+4).text()));
-//        kyrgystan.setTotalRecovered(checkForNull(doc.getElementsByTag("td").get(country+5).text()));
-//        kyrgystan.setActiveCases(checkForNull(doc.getElementsByTag("td").get(country+6).text()));
-        for (Article a: articles){
-            if(a.getCountry().contains("Russia"))
-                return a;
-        }
-        return null;
+        List<String> list = Changer.russiaList();
+        Article article = new Article(
+                list.get(0),
+                list.get(1),
+                checkForNull(list.get(2)),
+                list.get(3),
+                checkForNull(list.get(4)),
+                list.get(5),
+                list.get(6)
+        );
+        return article;
     }
+
+
 
     public static String checkForNull(String str){
         if (str.equals("")){
